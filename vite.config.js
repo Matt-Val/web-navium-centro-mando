273c8a7cfeabca3c4447e3b8f95fb5e8c807718d
Matt-Val <mat.ramosv@duocuc.cl>
2026-06-19 23:38:43 -0400

    config: configuracion de dependencias

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Redirige las llamadas de autenticación al microservicio de usuarios
+      '/api/auth': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+      },
+      // Redirige las llamadas del dashboard al BFF
+      '/api/dashboard': {
+        target: 'http://localhost:8084',
+        changeOrigin: true,
+      }
+    }
+  }
 })
